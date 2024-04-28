71a54345ea3ecf62e982a653caa7070f2de59cdc
guzel <guzel.kh23@gmail.ru>
2024-04-28 13:28:01 +0300

    22.1 1\1

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,12 +1,16 @@
 from django.urls import path
 
 from catalog.apps import CatalogConfig
-from catalog.views import contacts, ProductListView, ProductDetailView
+from catalog.views import contacts, ProductListView, ProductDetailView, ProductCreateView, ProductUpdateView, \
+    ProductDeleteView
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
-    path('', ProductListView.as_view(), name='index'),
+    path('create/', ProductCreateView.as_view(), name='create'),
+    path('', ProductListView.as_view(), name='list'),
     path('contacts/', contacts, name='contacts'),
-    path('view/<int:pk>/catalog/', ProductDetailView.as_view(), name='product_detail')
+    path('view/<int:pk>/catalog/', ProductDetailView.as_view(), name='view'),
+    path('edit/<int:pk>/', ProductUpdateView.as_view(), name='edit'),
+    path('delete/<int:pk>/', ProductDeleteView.as_view(), name='delete'),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,12 +1,19 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, DeleteView, CreateView, UpdateView
 
 from catalog.models import Product
 
 
+class ProductCreateView(CreateView):
+    model = Product
+    fields = ('name', 'description', 'preview', 'price')
+    # form_class = ProductForm
+    success_url = reverse_lazy('catalog:list')
+
+
 class ProductListView(ListView):
     model = Product
-    template_name = 'catalog/index.html'
 
 
 # def index(request):
@@ -31,6 +38,23 @@ def contacts(request):
     return render(request, 'catalog/contacts.html', context)
 
 
+class ProductUpdateView(UpdateView):
+    model = Product
+    fields = ('name', 'description', 'preview', 'price')
+    # form_class = ProductForm
+    success_url = reverse_lazy('catalog:list')
+
+# def toggle_activity(request, pk):
+#     product_item = get_object_or_404(Product,pk=pk)
+#     if product_item.is_active:
+#         product_item.is_active = False
+#     else:
+#         product_item.is_active = True
+#     product_item.save()
+#
+#     return redirect(reverse('catalog:index'))
+
+
 class ProductDetailView(DetailView):
     model = Product
 
@@ -42,4 +66,32 @@ class ProductDetailView(DetailView):
 #         'title': f'Вы выбрали {product_item.name}'
 #     }
 #     return render(request, 'catalog/product.html', context)
+
+
+class ProductDeleteView(DeleteView):
+    model = Product
+    success_url = reverse_lazy('catalog:list')
+
+
+
+
+
+
+
+# class VersionCreateView(CreateView):
+#     model = Version
+#     # form_class = VersionForm
+#     success_url = reverse_lazy('catalog:index')
+#
+#     # def form_valid(self, form):
+#     #     product_pk = self.kwargs['pk']  # Получаем pk продукта из URL
+#     #     product = Product.objects.get(pk=product_pk)  # Получаем объект продукта
+#     #     form.instance.product = product  # Устанавливаем продукт в поле версии
+#     #     return super().form_valid(form)
+#
+#
+# class VersionListView(ListView):
+#     model = Product
+#     # extra_context = {'title': 'Продукты'}
+
 
