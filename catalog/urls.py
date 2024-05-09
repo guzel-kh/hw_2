ca04ce14522d6e764def462e61157c814647ee89
guzel <guzel.kh23@gmail.ru>
2024-05-09 15:08:20 +0300

    23.2 2 fixed after checking

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.views.decorators.cache import cache_page
 
 from catalog.apps import CatalogConfig
 from catalog.views import contacts, ProductListView, ProductDetailView, ProductCreateView, ProductUpdateView, \
@@ -10,7 +11,7 @@ urlpatterns = [
     path('create/', ProductCreateView.as_view(), name='create'),
     path('', ProductListView.as_view(), name='list'),
     path('contacts/', contacts, name='contacts'),
-    path('view/<int:pk>/catalog/', ProductDetailView.as_view(), name='view'),
+    path('view/<int:pk>/catalog/', cache_page(60)(ProductDetailView.as_view()), name='view'),
     path('edit/<int:pk>/', ProductUpdateView.as_view(), name='edit'),
     path('delete/<int:pk>/', ProductDeleteView.as_view(), name='delete'),
     path('publish/<int:pk>/', toggle_publish, name='toggle_publish'),
